6a23a66559938ac887a3704f5110b12be781ed50
AjeetCode <ajeet.iet.ru@gmail.com>
2026-03-19 16:55:00 +0530

    Time: 1 ms (99.96%), Space: 46.7 MB (90.28%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=0;
+        for(int num:nums){
+            n=n^num;
+        }
+        return n;
+    }
+}
+
+
+
+
+
+//     public int singleNumber(int[] nums) {
+//         int n=0;
+//         for(int num:nums){
+//             n=n^num;
+//         }
+//         return n;
+//     }
+// }
