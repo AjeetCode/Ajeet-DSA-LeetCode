e99dd6321aa3ae117ffc430572276c11dd61710a
AjeetCode <ajeet.iet.ru@gmail.com>
2026-04-15 10:26:26 +0530

    Time: 30 ms (64.39%), Space: 53.7 MB (80.98%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         // maxHeap (a,b)->b-a
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[0]*b[0]+b[1]*b[1]-(a[0]*a[0]+a[1]*a[1])));
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->((b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1])));
         for(int[] point:points){
             pq.add(point);
             if(pq.size()>k){
