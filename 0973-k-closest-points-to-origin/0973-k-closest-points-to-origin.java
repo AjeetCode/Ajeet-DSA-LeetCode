be571bda325abf98da4cff51658243cebec847b8
AjeetCode <ajeet.iet.ru@gmail.com>
2026-05-30 18:40:28 +0530

    Time: 2605 ms (5.02%), Space: 56.8 MB (28.84%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -5,37 +5,26 @@ class Solution {
         // hashmap
         int l=points.length;
         HashMap<Integer, Double> hm=new HashMap<>();
-        // PriorityQueue<Double> pq=new PriorityQueue<>(Collections.reverseOrder());
+
+        PriorityQueue<Double> pq=new PriorityQueue<>(Collections.reverseOrder());
         int i=0;
         for(int[] point : points){
             Double distance=(Double)Math.sqrt(Math.pow(point[0],2)+Math.pow(point[1],2));
-            // pq.add(distance);
-            // if(pq.size()>k){
-            //     pq.poll();
-            // }
-            hm.put(i,distance);
-            i++;
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Double.compare(hm.get(b), hm.get(a)));
-        for(int j=0;j<l;j++){
-            pq.add(j);
+            pq.add(distance);
             if(pq.size()>k){
                 pq.poll();
             }
+            hm.put(i,distance);
+            i++;
         }
-        int ans[][] = new int[k][2];
-        // for(int j=0;j<k;j++){
-        //     int e=pq.poll();
-        //     int arr[]=points[e];
-        //     ans[j]=arr;
-        // }
-        int j = 0;
-        while(!pq.isEmpty()){
-            int e=pq.poll();
-            ans[j]=points[e];
-            j++;
-
-        }
+        int j =0;
+        int[][] ans =  new int[k][2];
+        for (Integer key : hm.keySet()) {
+            if(pq.contains(hm.get(key))){
+                ans[j] = points[key];
+                j++;
+            }
+        }   
         return ans;
     }
 }
